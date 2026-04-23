013a0f67b5a78d91f77736c672032263d4ae1cc9
Lucas Miranda <lucas.miranda@starkbank.com>
2026-04-23 15:13:49 -0300

    Bump to v2.3.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author_email="developers@starkbank.com",
     keywords=["ecdsa", "elliptic curve", "elliptic", "curve", "stark bank", "starkbank", "cryptograph", "secp256k1", "prime256v1"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    version="2.2.0"
+    version="2.3.0"
 )
 
 
